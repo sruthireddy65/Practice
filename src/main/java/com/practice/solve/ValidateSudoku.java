03e96e3a52bb095aafd60c97f791742760aebdd4
Sruthi Kovvur <sruthikovvur@gmail.com>
2022-02-18 12:10:26 +0530

    refactor sudoku validator

--- src/main/java/com/practice/solve/ValidateSudoku.java
+++ src/main/java/com/practice/solve/ValidateSudoku.java
@@ -1,20 +1,22 @@
 package com.practice.solve;
 
-import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ValidateSudoku {
 
-    public boolean isValidSudoku(int[][] a){
-        if(isRowsValid(a) && isColumnsValid(a) && is3_3MatricesValid(a)){
+    public boolean isValidSudoku(int[][] a) {
+        if (isRowsValid(a) && isColumnsValid(a) && is3_3MatricesValid(a)) {
             return true;
         }
         return false;
     }
 
     private boolean is3_3MatricesValid(int[][] a) {
-        for(int i=0; i<9; i+=3){
-            for(int j=0; j<9; j+=3){
-                if(!isValidMatrix(i,j,a)){
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (!isValidMatrix(i, j, a)) {
                     return false;
                 }
             }
@@ -23,39 +25,42 @@ public class ValidateSudoku {
     }
 
     private boolean isValidMatrix(int row, int col, int[][] a) {
-        HashSet<Integer> ints = new HashSet<>();
-        for(int i=row; i<row+3; i++){
-            for(int j=col; j<col+3; j++){
-                if(ints.contains(a[i][j]) || a[i][j] == 0){
+        Set<Integer> ints = IntStream.rangeClosed(1, a.length).mapToObj(Integer::valueOf).collect(Collectors.toSet());
+        for (int i = row; i < row + 3; i++) {
+            for (int j = col; j < col + 3; j++) {
+                if (!ints.remove(a[i][j])) {
                     return false;
                 }
-                ints.add(a[i][j]);
             }
         }
         return true;
     }
 
     private boolean isColumnsValid(int[][] a) {
-        for(int i=0; i<9; i++){
-            HashSet<Integer> ints = new HashSet<>();
-            for(int j=0; j<9; j++){
-                if(ints.contains(a[j][i]) || a[j][i] == 0){
+        for (int i = 0; i < a.length; i++) {
+            Set<Integer> ints = IntStream.rangeClosed(1, a.length).mapToObj(Integer::valueOf).collect(Collectors.toSet());
+            for (int j = 0; j < a.length; j++) {
+                if (!ints.remove(a[j][i])) {
                     return false;
                 }
-                ints.add(a[j][i]);
+            }
+            if (!ints.isEmpty()) {
+                return false;
             }
         }
         return true;
     }
 
     private boolean isRowsValid(int[][] a) {
-        for(int i=0; i<9; i++){
-            HashSet<Integer> ints = new HashSet<>();
-            for(int j=0; j<9; j++){
-                if(ints.contains(a[i][j]) || a[i][j] == 0){
+        for (int i = 0; i < a.length; i++) {
+            Set<Integer> ints = IntStream.rangeClosed(1, a.length).mapToObj(Integer::valueOf).collect(Collectors.toSet());
+            for (int j = 0; j < a.length; j++) {
+                if (!ints.remove(a[i][j])) {
                     return false;
                 }
-                ints.add(a[i][j]);
+            }
+            if (!ints.isEmpty()) {
+                return false;
             }
         }
         return true;
